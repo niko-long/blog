d5e33339fe6e79bb5c307bf96f4f1cdf8579afba
nikolong <nikolong@tencent.com>
2026-06-11 18:33:34 +0800

    1

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -573,6 +573,9 @@ export default hopeTheme({
 // 此处开启了很多功能用于演示，你应仅保留用到的功能。
     markdown: {
         align: true,
+        math: {
+            type: "katex",
+        },
         attrs:
             true,
         codeTabs:
